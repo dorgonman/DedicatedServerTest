dc0e5d250bcde8f99f0e2e429942a1d122782f6d
dorgon <dorgonman@hotmail.com>
2016-10-17 15:06:29 +0800

    init commit

---
+++ Source/DedicatedServerTest/DedicatedServerTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DedicatedServerTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERTEST_API ADedicatedServerTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
