4bc7329e363abbd014bd15d884ca46485afc9435
dorgon <dorgonman@hotmail.com>
2016-10-17 18:41:37 +0800

    UPDATE

--- ci_scripts/job/sh/win64/build_server_development_job.sh
+++ ci_scripts/job/sh/win64/build_server_development_job.sh
@@ -69,7 +69,7 @@ echo {BUILD_CONFIG}: ${BUILD_CONFIG}
 echo {PLATFORM}: 	${PLATFORM}
 echo {PROJECT_FILE}: ${PROJECT_FILE}
 
-./git_clean.sh
+#./git_clean.sh
 python -u ${PROJECT_ROOT}/ci_scripts/function/python/HorizonBuildTool/HorizonBuildTool/Source/HorizonUE4Build/Main.py \
 	 --buildserver  \
 	 --engine "${UE4_ENGINE_ROOT}" \
